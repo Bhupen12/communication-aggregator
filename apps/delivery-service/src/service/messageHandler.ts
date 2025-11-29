b877b981f310ef3776b2ca37f8eb0793a1f5777e
BhupenSahoo-Suraksha <160491383+BhupenSahoo-Suraksha@users.noreply.github.com>
2025-11-29 13:50:07 +0530

    refactor delivery service

---
+++ apps/delivery-service/src/service/messageHandler.ts
@@ -0,0 +1,86 @@
+import { Message } from "amqplib";
+import { LogMessagePayload, QUEUES, SERVICE_NAMES, TaskMessagePayload } from "@repo/shared";
+import { saveDeliveryLog } from "./db";
+import { parseTaskMessage, simulateDelivery } from "./utils";
+
+const MAX_RETRIES = 3;
+
+export const handleTaskMessage = async (channel: any, msg: Message) => {
+  const headers = msg.properties?.headers || {};
+  const retryCount = headers.retryCount || 0;
+
+  const data = parseTaskMessage(msg);
+  if (!data) {
+    console.error("Dropped invalid message");
+    channel.ack(msg);
+    return;
+  }
+
+  console.log("Received message:", data);
+
+  try {
+    await simulateDelivery(data.type, data.to);
+
+    const logPayload: LogMessagePayload = {
+      service: SERVICE_NAMES.DELIVERY,
+      taskId: data.id,
+      to: data.to,
+      type: data.type,
+      status: "SENT",
+      timestamp: new Date(),
+    };
+
+    channel.sendToQueue(
+      QUEUES.LOGS, 
+      Buffer.from(JSON.stringify(logPayload)), 
+      { persistent: true }
+    );
+
+    await saveDeliveryLog(data.id, data.type, data.to, "SENT");
+
+    channel.ack(msg);
+    return;
+  } catch (err) {
+    console.error("Delivery error:", err);
+  }
+
+  // ---------- RETRY LOGIC ----------
+  if (retryCount < MAX_RETRIES) {
+    const updatedRetry = retryCount + 1;
+
+    console.log(`Retrying ${data.id} (${updatedRetry}/${MAX_RETRIES})`);
+
+    channel.sendToQueue(
+      QUEUES.MESSAGE, 
+      msg.content, 
+      {
+        persistent: true,
+        headers: { retryCount: updatedRetry },
+      }
+    );
+
+    channel.ack(msg);
+    return;
+  }
+
+  // ---------- FINAL FAILURE ----------
+  console.log(`Final failure for task ${data.id}`);
+
+  const failPayload: LogMessagePayload = {
+    service: SERVICE_NAMES.DELIVERY,
+    taskId: data.id,
+    to: data.to,
+    type: data.type,
+    status: "FAILED",
+    timestamp: new Date(),
+  };
+
+  channel.sendToQueue(
+    QUEUES.LOGS, 
+    Buffer.from(JSON.stringify(failPayload)), 
+    { persistent: true });
+
+  await saveDeliveryLog(data.id, data.type, data.to, "FAILED");
+
+  channel.ack(msg);
+};
